e81aa1f5fc177ba2ac27c924c05e20befe392c50
Redoxahmii <fullmetal2334@gmail.com>
2024-06-27 21:46:28 +0500

    fix(home):fix sizes

--- src/components/featured-prompts/index.jsx
+++ src/components/featured-prompts/index.jsx
@@ -56,7 +56,7 @@ const FeaturedPrompts = () => {
     },
   ]);
   return (
-    <div className="mt-[70vh]">
+    <div className="mt-[92vh]">
       <Grid container spacing={0}>
         <Grid item xs={1} sm={1} md={1} lg={1} xl={1}></Grid>
         <Grid xs={10} sm={10} md={10} lg={10} xl={10}>
